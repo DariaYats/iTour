2a79da3ef255d9277ff860cd8fe9949aa82f63b2
Daria Yats <dariayatsyniuk@gmail.com>
2024-07-18 15:11:44 +0200

    Initial Commit

---
+++ iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by Дарья Яцынюк on 18.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
