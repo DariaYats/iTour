911cc51e5a6ab16d5c590978357a4b511106fd17
Daria Yats <dariayatsyniuk@gmail.com>
2024-07-19 14:51:00 +0200

    create data model

---
+++ iTour/Destination.swift
@@ -0,0 +1,24 @@
+//
+//  Destination.swift
+//  iTour
+//
+//  Created by Дарья Яцынюк on 18.07.2024.
+//
+
+import SwiftData
+import Foundation
+
+@Model
+class Destination {
+    var name: String
+    var details: String
+    var date: Date
+    var priority: Int
+    
+    init(name: String = "", details: String = "", date: Date = .now, priority: Int = 2) {
+        self.name = name
+        self.details = details
+        self.date = date
+        self.priority = priority
+    }
+}
